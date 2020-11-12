d455abdc3d02faec0ef472cf7cd9946e0d5fc71e
Dwayne <deandraper82@gmail.com>
2020-11-11 21:55:03 -0600

    Pushing for the night. Need to add final touches to app.js to complete next milestone.

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -2,6 +2,40 @@ const models = require('../models');
 const Promise = require('bluebird');
 
 module.exports.createSession = (req, res, next) => {
+  if (req.cookies.shortlyid === undefined) {
+    models.Sessions.create().
+      then((sessionPromise) => {
+        let id = sessionPromise.insertId;
+        models.Sessions.get({id}).
+          then((session) => {
+            req.session = session;
+            res.cookie('shortlyid', session.hash);
+            next();
+          });
+      });
+  } else {
+    let options = {};
+    options.hash = req.cookies.shortlyid;
+    models.Sessions.get(options).
+      then((session) => {
+        if (session === undefined) {
+          models.Sessions.create().
+            then((sessionPromise) => {
+              let id = sessionPromise.insertId;
+              models.Sessions.get({id}).
+                then((session) => {
+                  req.session = session;
+                  res.cookie('shortlyid', session.hash);
+                  next();
+                });
+            });
+        } else {
+          req.session = session;
+          res.cookie('shortlyid', session.hash);
+          next();
+        }
+      });
+  }
 };
 
 /************************************************************/
--- server/middleware/cookieParser.js
+++ server/middleware/cookieParser.js
@@ -1,17 +1,15 @@
-const models = require('../models');
-
 const parseCookies = (req, res, next) => {
-  console.log('Req object:', req.headers.cookie);
+  // console.log('Req object:', req.headers.cookie);
   let cookieObject = {};
   if (req.headers.cookie) {
     let splitCookies = req.headers.cookie.split('; ');
-    console.log('splitCookies: ', splitCookies);
+    // console.log('splitCookies: ', splitCookies);
     for (let i = 0; i < splitCookies.length; i++) {
       let indCookieArray = splitCookies[i].split('=');
       cookieObject[indCookieArray[0]] = indCookieArray[1];
     }
   }
-  console.log('Parsed cookie: ', cookieObject);
+  // console.log('Parsed cookie: ', cookieObject);
   req.cookies = cookieObject;
   next();
 };
